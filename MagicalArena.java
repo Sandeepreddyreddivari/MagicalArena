afdd9c24cf5e3daa7853b53896d6d75c165748b9
Sandeepreddyreddivari <124238382+Sandeepreddyreddivari@users.noreply.github.com>
2024-05-04 22:30:27 +0530

    Create MagicalArena.java file with game introduction

---
+++ MagicalArena.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to MAGICAL ARENA!!\n"+"Rules:");
+        System.out.println("**Every Player has HEALTH,STRENGTH and an ATTACK Powers.\n"+"**Attacking player rolls the attacking dice and the defending player rolls the defending dice.\n"+ "**The ATTACK  value multiplied by the outcome of the  attacking dice roll is the damage created by the attacker.\n"+"**The defender Strength value, multiplied by the outcome of the defending dice is the damage defended by the defender.\n"+ "**Whatever damage created by attacker which is in excess of the damage defended by the defender will reduce the HEALTH of the defender\n"+"**The player dies if his HEALTH touches 0.\n"+"Good Luck,Have a nice game");
+    }
+}
